43dcf31cecb041316bc30c981a442ad18da7d485
Jordan Ogilvy <jo43@waikato.ac.nz>
2016-08-20 16:08:55 +1200

    added BufferedReader to ClientMain, loops in main for messages from the server

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 /**
@@ -14,7 +16,14 @@ public class ClientMain {
             try{
                 //connect to the server
                 Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
-
+                //reader to read lines of text from the server
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //loop for messages from the server
+                while(true){
+                    if(reader.ready()){
+                        System.out.println(reader.readLine());
+                    }
+                }
 
             }catch(IOException ioe){
                 ioe.printStackTrace();
