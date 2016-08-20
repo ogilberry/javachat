3b810d9dabb16135f583f433c038254378219cb9
Jordan Ogilvy <jo43@waikato.ac.nz>
2016-08-20 12:38:16 +1200

    created IntelliJ project, ClientGUI.java javafx skeleton

---
+++ src/ClientGUI.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Jordan on 20-Aug-16.
+ */
+public class ClientGUI extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
